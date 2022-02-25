3fae6b1866be451edf5ea6f0f6e05175ad09f8d7
anna <osipovaanna99_99@mail.ru>
2022-02-25 09:17:27 +0300

    2

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace ConsoleApp1
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result;
+            NumberManipulater numberManipulater = new NumberManipulater();
+
+
+            result = numberManipulater.FindMax(20, 30);
+
+            Console.WriteLine(result);
+
+        }
+    }
+}
+
